6f100b419e4fe52793b033f1967bbf10b447c845
kelvin wepo <kelvinwepo7710@gmail.com>
2024-09-18 22:27:43 +0300

    feat: this commit defines User_management app in settings.py

--- backend/clean_connect/settings.py
+++ backend/clean_connect/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
+    'user_management',
 ]
 
 MIDDLEWARE = [
